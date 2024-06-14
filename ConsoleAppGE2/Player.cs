cbd75b5747e712b3f4431ba5957935e23b223f0a
S-JOBI23-055\student <231105@st.yoshida-g.ac.jp>
2024-06-14 09:53:25 +0900

    Playerクラスを作成 #2

---
+++ ConsoleAppGE2/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGE2
+{
+    internal class Player
+    {
+        //-----------------------------
+        // フィールド
+
+        /// <summary>
+        /// 名前
+        /// </summary>
+        private string name;
+
+        /// <summary>
+        /// 体力
+        /// </summary>
+        private int hp;
+
+        /// <summary>
+        /// 攻撃力
+        /// </summary>
+        private int attack;
+    }
+}
